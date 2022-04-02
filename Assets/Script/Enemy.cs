42f8da2e4090ffebcf75a5e0d1ffa8e98a271afd
AustinMaZW <auzzyma@gmail.com>
2022-04-02 08:18:11 -0700

    first commit

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Enemy Stats")]
+    [SerializeField] float health = 100;
+    [SerializeField] int scoreValue = 150;
+
+    [Header("Shooting")]
+    float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject projectile;
+    [SerializeField] float projectileSpeed = 10f;
+
+    [Header("Sound Effects")]
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float durationOfExplosion = 1f;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] [Range (0,1)]float deathSoundVolume = 0.7f;
+    [SerializeField] AudioClip shootSound;
+    [SerializeField] [Range(0, 1)] float shootSoundVolume = 0.3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;  //shotCounter goes down in time each frame for however long our frame takes
+        if (shotCounter <=0f)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        // instantiate a laser projectile, Quaternion.identity means use current rotation
+        GameObject laser = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject; 
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+        AudioSource.PlayClipAtPoint(shootSound, Camera.main.transform.position, shootSoundVolume); //shoot sound
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        FindObjectOfType<GameSession>().AddToScore(scoreValue);
+        GameObject explosion = Instantiate(deathVFX, transform.position, Quaternion.identity) as GameObject;
+        Destroy(gameObject);
+        Destroy(explosion, durationOfExplosion);
+        AudioSource.PlayClipAtPoint(deathSound, Camera.main.transform.position, deathSoundVolume); //death sound
+    }
+}
---
+++ Assets/Script/backgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundScroller : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.02f;
+    Material myMaterial;
+    Vector2 offSet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;     // get the renderer component
+        offSet = new Vector2(0f, backgroundScrollSpeed);    // x coor doesn't change since we are only scrolling vertically
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
